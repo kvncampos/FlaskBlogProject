bae2c253acfdec0bf19e2485e44e33d187278e86
kvncampos <kvnelpdfw@hotmail.com>
2024-01-17 21:17:42 -0600

    finished blog

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/de00a5a729f58031eede").json()
+OWN_EMAIL = YOUR OWN EMAIL ADDRESS
+OWN_PASSWORD = YOUR EMAIL ADDRESS PASSWORD
+
 
 app = Flask(__name__)
 
@@ -21,6 +24,22 @@ def about():
 def contact():
     return render_template("contact.html")
 
+
+@app.route("/form-entry", methods=["POST"])
+def form_data():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["msg"])
+        print(data["name"], data["email"], data["phone"], data["msg"])
+        return "Form Filled Out Successfully!"
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 @app.route("/post/<int:index>")
 def show_post(index):
